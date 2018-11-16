20fba9416ef7a3958a2830c3a2fde7bda321697a
Frank Mueller <mue@tideland.biz>
2018-11-16 22:19:29 +0100

    Port environments from goaudit

---
+++ audit/environments/doc.go
@@ -0,0 +1,12 @@
+// Tideland Go Library - Audit - Environments
+//
+// Copyright (C) 2012-2018 Frank Mueller / Tideland / Oldenburg / Germany
+//
+// All rights reserved. Use of this source code is governed
+// by the new BSD license.
+
+// Package environments of Tideland Go Library Audit helps to manipulate
+// environment variables or create temporary directories for unit tests.
+package environments
+
+// EOF
