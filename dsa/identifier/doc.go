dfeb9ff8b676d69f6c32dfb74b9f5563c1939f5e
Frank Mueller <frank@mweb.de>
2018-11-24 16:19:08 +0100

    Harmonized package documentation

--- dsa/identifier/doc.go
+++ dsa/identifier/doc.go
@@ -5,14 +5,12 @@
 // All rights reserved. Use of this source code is governed
 // by the new BSD license.
 
-// Package identifier of the Tideland Go Library provides different ways to
-// produce identifiers out of diffent input data as well as UUIDs.
-//
-// The UUID generation can be done according the versions 1, 3, 4, and 5.
-// Other identifier types are based on passed data or types. Here
-// the individual parts are harmonized and concatenated by the
-// passed seperators. It is the users responsibility to check if
-// the identifier is unique in its context.
+// Package identifier provides different ways to produce identifiers out
+// of diffent parts. It also contains a UUID generation.This can be done
+// according the versions 1, 3, 4, and 5. Other identifier types are based
+// on passed data or types. Here the individual parts are harmonized and
+// concatenated by the passed seperators. It is the users responsibility
+// to check if the identifier is unique in its context.
 package identifier
 
 // EOF
