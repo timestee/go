a1f914fb693cdc0648fcb490affd0d4ef14b39b1
Frank Mueller <frank@mweb.de>
2018-12-03 15:16:21 +0100

    Reorganize file content

--- audit/web/io.go
+++ audit/web/io.go
@@ -24,6 +24,83 @@ import (
 )
 
 //--------------------
+// CONSTANTS
+//--------------------
+
+const (
+	HeaderAccept      = "Accept"
+	HeaderContentType = "Content-Type"
+
+	ApplicationJSON = "application/json"
+	ApplicationXML  = "application/xml"
+)
+
+//--------------------
+// VALUES
+//--------------------
+
+// Values wraps the query and cookie values for easy assertion
+// and access.
+type Values struct {
+	assert *asserts.Asserts
+	values url.Values
+}
+
+// NewValues creates a new values instance.
+func NewValues(assert *asserts.Asserts, values url.Values) *Values {
+	vs := &Values{
+		assert: assert,
+		values: values,
+	}
+	if vs.values == nil {
+		vs.values = url.Values{}
+	}
+	return vs
+}
+
+// Add adds or appends a value to a named field.
+func (vs *Values) Add(key, value string) {
+	vsvs, ok := v.values[key]
+	if ok {
+		vs.values[key] = append(vsvs, value)
+	} else {
+		vs.values[key] = []string{value}
+	}
+}
+
+// Values returns the inner values for direct usage.
+func (vs *Values) Values() url.Values {
+	return vs.values
+}
+
+// AssertKeyExists tests if the values contain the passed key.
+func (vs *Values) AssertKeyExists(key string, msgs ...string) {
+	restore := v.assert.IncrCallstackOffset()
+	defer restore()
+	_, ok := vs.values[key]
+	v.assert.True(ok, msgs...)
+}
+
+// AssertKeyContainsValue tests if the values contain the passed key
+// and that the passed value.
+func (vs *Values) AssertKeyContainsValue(key, expected string, msgs ...string) {
+	restore := v.assert.IncrCallstackOffset()
+	defer restore()
+	vsvs, ok := vs.values[key]
+	v.assert.True(ok, msgs...)
+	v.assert.Contents(expected, vsvs, msgs...)
+}
+
+// AssertKeyValueEquals tests if the key value equals the expected value.
+func (vs *Values) AssertKeyValueEquals(key, expected string, msgs ...string) {
+	restore := v.assert.IncrCallstackOffset()
+	defer restore()
+	vsvs, ok := vs.values[key]
+	v.assert.True(ok, msgs...)
+	v.assert.Equal(vs.values.Get(key), expected, msgs...)
+}
+
+//--------------------
 // REQUEST
 //--------------------
 
