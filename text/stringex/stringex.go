cfc0b294f672807031c46a5d184d23c348d4dfb2
Frank Mueller <mail@themue.dev>
2019-06-12 22:48:40 +0200

    Split Matches() into more modular type to reduce complexity

--- text/stringex/stringex.go
+++ text/stringex/stringex.go
@@ -13,7 +13,6 @@ package stringex // import "tideland.dev/go/text/stringex"
 
 import (
 	"strings"
-	"unicode"
 )
 
 //--------------------
@@ -65,145 +64,199 @@ func SplitMap(s, sep string, m func(p string) (string, bool)) []string {
 // MATCHER
 //--------------------
 
-// Matches checks if the pattern matches a given value. Here
-// ? matches one char, * a group of chars, [any] any of these
-// chars, and [0-9] any of this char range. The latter can
-// be negotiated with [^abc] and \ escapes the pattern chars.
-func Matches(pattern, value string, ignoreCase bool) bool {
-	patternRunes := append([]rune(pattern), '\u0000')
-	patternLen := len(patternRunes) - 1
-	patternPos := 0
-	valueRunes := append([]rune(value), '\u0000')
-	valueLen := len(valueRunes) - 1
-	valuePos := 0
-	for patternLen > 0 {
-		switch patternRunes[patternPos] {
-		case '*':
-			// Asterisk for group of characters.
-			for patternRunes[patternPos+1] == '*' {
-				patternPos++
-				patternLen--
-			}
-			if patternLen == 1 {
-				return true
-			}
-			for valueLen > 0 {
-				patternCopy := make([]rune, len(patternRunes[patternPos+1:]))
-				valueCopy := make([]rune, len(valueRunes[valuePos:]))
-				copy(patternCopy, patternRunes[patternPos+1:])
-				copy(valueCopy, valueRunes[valuePos:])
-				if Matches(string(patternCopy), string(valueCopy), ignoreCase) {
-					return true
-				}
-				valuePos++
-				valueLen--
-			}
-			return false
-		case '?':
-			// Question mark for one character.
-			if valueLen == 0 {
-				return false
-			}
-			valuePos++
-			valueLen--
-		case '[':
-			// Square brackets for groups of valid characters.
-			patternPos++
-			patternLen--
-			not := (patternRunes[patternPos] == '^')
-			match := false
-			if not {
-				patternPos++
-				patternLen--
-			}
-		group:
-			for {
-				switch {
-				case patternRunes[patternPos] == '\\':
-					patternPos++
-					patternLen--
-					if patternRunes[patternPos] == valueRunes[valuePos] {
-						match = true
-					}
-				case patternRunes[patternPos] == ']':
-					break group
-				case patternLen == 0:
-					patternPos--
-					patternLen++
-					break group
-				case patternRunes[patternPos+1] == '-' && patternLen >= 3:
-					start := patternRunes[patternPos]
-					end := patternRunes[patternPos+2]
-					vr := valueRunes[valuePos]
-					if start > end {
-						start, end = end, start
-					}
-					if ignoreCase {
-						start = unicode.ToLower(start)
-						end = unicode.ToLower(end)
-						vr = unicode.ToLower(vr)
-					}
-					patternPos += 2
-					patternLen -= 2
-					if vr >= start && vr <= end {
-						match = true
-					}
-				default:
-					if !ignoreCase {
-						if patternRunes[patternPos] == valueRunes[valuePos] {
-							match = true
-						}
-					} else {
-						if unicode.ToLower(patternRunes[patternPos]) == unicode.ToLower(valueRunes[valuePos]) {
-							match = true
-						}
-					}
-				}
-				patternPos++
-				patternLen--
+const (
+	matchSuccess = 1
+	matchCont    = 0
+	matchFail    = -1
+)
 
-			}
-			if not {
-				match = !match
-			}
-			if !match {
-				return false
-			}
-			valuePos++
-			valueLen--
-		case '\\':
-			if patternLen >= 2 {
-				patternPos++
-				patternLen--
-			}
-			fallthrough
-		default:
-			if !ignoreCase {
-				if patternRunes[patternPos] != valueRunes[valuePos] {
-					return false
-				}
-			} else {
-				if unicode.ToLower(patternRunes[patternPos]) != unicode.ToLower(valueRunes[valuePos]) {
-					return false
-				}
-			}
-			valuePos++
-			valueLen--
+// matcher is a helper type for string pattern matching.
+type matcher struct {
+	patternRunes []rune
+	patternLen   int
+	patternPos   int
+	valueRunes   []rune
+	valueLen     int
+	valuePos     int
+}
+
+// newMatcher creates the helper type for string pattern matching.
+func newMatcher(pattern, value string, ignoreCase bool) *matcher {
+	if ignoreCase {
+		return newMatcher(strings.ToLower(pattern), strings.ToLower(value), false)
+	}
+	prs := append([]rune(pattern), '\u0000')
+	vrs := append([]rune(value), '\u0000')
+	return &matcher{
+		patternRunes: prs,
+		patternLen:   len(prs) - 1,
+		patternPos:   0,
+		valueRunes:   vrs,
+		valueLen:     len(vrs) - 1,
+		valuePos:     0,
+	}
+}
+
+// matches checks if the value matches the pattern.
+func (m *matcher) matches() bool {
+	// Loop over the pattern.
+	for m.patternLen > 0 {
+		switch m.processPatternRune() {
+		case matchSuccess:
+			return true
+		case matchFail:
+			return false
+
 		}
-		patternPos++
-		patternLen--
-		if valueLen == 0 {
-			for patternRunes[patternPos] == '*' {
-				patternPos++
-				patternLen--
+		m.patternPos++
+		m.patternLen--
+		if m.valueLen == 0 {
+			for m.patternRunes[m.patternPos] == '*' {
+				m.patternPos++
+				m.patternLen--
 			}
 			break
 		}
 	}
-	if patternLen == 0 && valueLen == 0 {
+	if m.patternLen == 0 && m.valueLen == 0 {
 		return true
 	}
 	return false
+}
+
+// processPatternRune handles the current leading pattern rune.
+func (m *matcher) processPatternRune() int {
+	switch m.patternRunes[m.patternPos] {
+	case '*':
+		return m.processAsterisk()
+	case '?':
+		return m.processQuestionMark()
+	case '[':
+		return m.processOpenBracket()
+	case '\\':
+		m.processBackslash()
+		fallthrough
+	default:
+		return m.processDefault()
+	}
+}
+
+// processAsterisk handles groups of characters.
+func (m *matcher) processAsterisk() int {
+	for m.patternRunes[m.patternPos+1] == '*' {
+		m.patternPos++
+		m.patternLen--
+	}
+	if m.patternLen == 1 {
+		return matchSuccess
+	}
+	for m.valueLen > 0 {
+		patternCopy := make([]rune, len(m.patternRunes[m.patternPos+1:]))
+		valueCopy := make([]rune, len(m.valueRunes[m.valuePos:]))
+		copy(patternCopy, m.patternRunes[m.patternPos+1:])
+		copy(valueCopy, m.valueRunes[m.valuePos:])
+		pam := newMatcher(string(patternCopy), string(valueCopy), false)
+		if pam.matches() {
+			return matchSuccess
+		}
+		m.valuePos++
+		m.valueLen--
+	}
+	return matchFail
+}
+
+// processQuestionMark handles a single character.
+func (m *matcher) processQuestionMark() int {
+	if m.valueLen == 0 {
+		return matchFail
+	}
+	m.valuePos++
+	m.valueLen--
+	return matchCont
+}
+
+// processOpenBracket handles an open bracket for a group of characters.
+func (m *matcher) processOpenBracket() int {
+	m.patternPos++
+	m.patternLen--
+	not := (m.patternRunes[m.patternPos] == '^')
+	match := false
+	if not {
+		m.patternPos++
+		m.patternLen--
+	}
+group:
+	for {
+		switch {
+		case m.patternRunes[m.patternPos] == '\\':
+			m.patternPos++
+			m.patternLen--
+			if m.patternRunes[m.patternPos] == m.valueRunes[m.valuePos] {
+				match = true
+			}
+		case m.patternRunes[m.patternPos] == ']':
+			break group
+		case m.patternLen == 0:
+			m.patternPos--
+			m.patternLen++
+			break group
+		case m.patternRunes[m.patternPos+1] == '-' && m.patternLen >= 3:
+			start := m.patternRunes[m.patternPos]
+			end := m.patternRunes[m.patternPos+2]
+			vr := m.valueRunes[m.valuePos]
+			if start > end {
+				start, end = end, start
+			}
+			m.patternPos += 2
+			m.patternLen -= 2
+			if vr >= start && vr <= end {
+				match = true
+			}
+		default:
+			if m.patternRunes[m.patternPos] == m.valueRunes[m.valuePos] {
+				match = true
+			}
+		}
+		m.patternPos++
+		m.patternLen--
+	}
+	if not {
+		match = !match
+	}
+	if !match {
+		return matchFail
+	}
+	m.valuePos++
+	m.valueLen--
+	return matchCont
+}
+
+// processBackslash handles escaping via baskslash.
+func (m *matcher) processBackslash() int {
+	if m.patternLen >= 2 {
+		m.patternPos++
+		m.patternLen--
+	}
+	return matchCont
+}
+
+// processDefault handles any other rune.
+func (m *matcher) processDefault() int {
+	if m.patternRunes[m.patternPos] != m.valueRunes[m.valuePos] {
+		return matchFail
+	}
+	m.valuePos++
+	m.valueLen--
+	return matchCont
+}
+
+// Matches checks if the pattern matches a given value. Here
+// ? matches one char, * a group of chars, [any] any of these
+// chars, and [0-9] any of this char range. Groups can
+// be negated with [^abc] and \ escapes pattern chars.
+func Matches(pattern, value string, ignoreCase bool) bool {
+	m := newMatcher(pattern, value, ignoreCase)
+	return m.matches()
 }
 
 // EOF
