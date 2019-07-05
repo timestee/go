7067823eb7f18f4d4fcd5f8ae3b85d6eaec274ea
Frank Mueller <mail@themue.dev>
2019-07-05 19:10:05 +0200

    Add more event runtime tests

--- together/events/runtime/runtime_test.go
+++ together/events/runtime/runtime_test.go
@@ -12,6 +12,7 @@ package runtime_test
 //--------------------
 
 import (
+	"fmt"
 	"testing"
 
 	"tideland.dev/go/audit/asserts"
@@ -22,16 +23,18 @@ import (
 // TESTS
 //--------------------
 
+// TestSpawnProcessors verifies starting the runtime, spawning some
+// processors, and stops the runtime.
 func TestSpawnProcessors(t *testing.T) {
 	assert := asserts.NewTesting(t, asserts.FailStop)
 	rt := runtime.New()
 
 	err := rt.SpawnProcessors(
-		NewTestProcessor("foo"),
-		NewTestProcessor("bar"),
-		NewTestProcessor("baz"),
+		NewTestProcessor("foo", nil),
+		NewTestProcessor("bar", nil),
+		NewTestProcessor("baz", nil),
 	)
-	assert.Nil(err)
+	assert.NoError(err)
 	ids := rt.ProcessorIDs()
 	assert.Length(ids, 3)
 	assert.Contains(ids, "foo")
@@ -39,7 +42,76 @@ func TestSpawnProcessors(t *testing.T) {
 	assert.Contains(ids, "baz")
 
 	err = rt.Stop()
-	assert.Nil(err)
+	assert.NoError(err)
+}
+
+// TestEmitEvents verifies emitting some events to an
+// individual
+func TestEmitEvents(t *testing.T) {
+	assert := asserts.NewTesting(t, asserts.FailStop)
+	rt := runtime.New()
+
+	datasC := make(chan []string, 1)
+	err := rt.SpawnProcessors(NewTestProcessor("foo", datasC))
+	assert.NoError(err)
+
+	rt.Emit("foo", runtime.Event{"add", "a"})
+	rt.Emit("foo", runtime.Event{"add", "b"})
+	rt.Emit("foo", runtime.Event{"add", "c"})
+	rt.Emit("foo", runtime.Event{"send", ""})
+
+	datas := <-datasC
+
+	assert.Length(datas, 3)
+
+	err = rt.Stop()
+	assert.NoError(err)
+}
+
+// TestSubscription verifies the subscription mechanis.
+func TestSubscription(t *testing.T) {
+	assert := asserts.NewTesting(t, asserts.FailStop)
+	rt := runtime.New()
+
+	fooDatasC := make(chan []string, 1)
+	barDatasC := make(chan []string, 1)
+	bazDatasC := make(chan []string, 1)
+	err := rt.SpawnProcessors(
+		NewTestProcessor("foo", fooDatasC),
+		NewTestProcessor("bar", barDatasC),
+		NewTestProcessor("baz", bazDatasC),
+	)
+	assert.NoError(err)
+	rt.Subscribe("foo", "bar")
+	rt.Subscribe("bar", "baz")
+
+	rt.Emit("foo", runtime.Event{"add", "a"})
+	rt.Emit("foo", runtime.Event{"add", "b"})
+	rt.Emit("foo", runtime.Event{"add", "c"})
+
+	rt.Emit("foo", runtime.Event{"length?", ""})
+
+	<-fooDatasC
+
+	rt.Emit("bar", runtime.Event{"send", ""})
+
+	datas := <-barDatasC
+
+	assert.Length(datas, 1)
+
+	rt.Emit("bar", runtime.Event{"length?", ""})
+
+	<-barDatasC
+
+	rt.Emit("baz", runtime.Event{"send", ""})
+
+	datas = <-bazDatasC
+
+	assert.Length(datas, 1)
+	assert.Equal(datas[0], `length "bar" = 1`)
+
+	err = rt.Stop()
+	assert.NoError(err)
 }
 
 //--------------------
@@ -49,12 +121,14 @@ func TestSpawnProcessors(t *testing.T) {
 type TestProcessor struct {
 	id      string
 	datas   []string
+	datasC  chan []string
 	emitter runtime.Emitter
 }
 
-func NewTestProcessor(id string) *TestProcessor {
+func NewTestProcessor(id string, datasC chan []string) *TestProcessor {
 	return &TestProcessor{
-		id: id,
+		id:     id,
+		datasC: datasC,
 	}
 }
 
@@ -72,7 +146,20 @@ func (tp *TestProcessor) Terminate() error {
 }
 
 func (tp *TestProcessor) Process(event runtime.Event) {
-
+	switch event.Topic {
+	case "add":
+		tp.datas = append(tp.datas, event.Data)
+	case "clear":
+		tp.datas = nil
+	case "length?":
+		tp.emitter.Emit(runtime.Event{
+			Topic: "add",
+			Data:  fmt.Sprintf("length %q = %d", tp.id, len(tp.datas)),
+		})
+		close(tp.datasC)
+	case "send":
+		tp.datasC <- tp.datas
+	}
 }
 
 func (tp *TestProcessor) Recover(r interface{}) error {
