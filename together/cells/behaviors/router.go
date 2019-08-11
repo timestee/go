af2ff5ed33388ebd89672b99d654e8dec4724059
Frank Mueller <mail@themue.dev>
2019-08-11 16:12:56 +0200

    Add router behavior

--- together/cells/behaviors/router.go
+++ together/cells/behaviors/router.go
@@ -18,57 +18,57 @@ import (
 )
 
 //--------------------
-// MESH ROUTER BEHAVIOR
+// ROUTER BEHAVIOR
 //--------------------
 
-// MeshRouter is a function type determining which cells shall get the event.
-type MeshRouter func(evt *event.Event) []string
+// Router is a function type determining which subscriber cells shall get the event.
+type Router func(evt *event.Event) []string
 
-// meshRouterBehavior check for each received event which cell will
+// routerBehavior check for each received event which subscriber will
 // get it based on the router function.
-type meshRouterBehavior struct {
+type routerBehavior struct {
 	id      string
-	routeTo MeshRouter
-	msh     *mesh.Mesh
+	emitter mesh.Emitter
+	routeTo Router
 }
 
-// NewMeshRouterBehavior creates a mesh router behavior using the passed function
-// to determine to which cells the received event shall be re-emitted.
-func NewMeshRouterBehavior(id string, router MeshRouter, msh *mesh.Mesh) mesh.Behavior {
-	return &meshRouterBehavior{
+// NewRouterBehavior creates a mesh router behavior using the passed function
+// to determine to which subscriber cells the received event shall be re-emitted.
+func NewRouterBehavior(id string, router Router) mesh.Behavior {
+	return &routerBehavior{
 		id:      id,
 		routeTo: router,
-		msh:     msh,
 	}
 }
 
 // ID returns the individual identifier of a behavior instance.
-func (b *meshRouterBehavior) ID() string {
+func (b *routerBehavior) ID() string {
 	return b.id
 }
 
 // Init the behavior.
-func (b *meshRouterBehavior) Init(emitter mesh.Emitter) error {
+func (b *routerBehavior) Init(emitter mesh.Emitter) error {
+	b.emitter = emitter
 	return nil
 }
 
 // Terminate the behavior.
-func (b *meshRouterBehavior) Terminate() error {
+func (b *routerBehavior) Terminate() error {
 	return nil
 }
 
 // Process emits the event to those ids returned by the router function.
-func (b *meshRouterBehavior) Process(evt *event.Event) error {
+func (b *routerBehavior) Process(evt *event.Event) error {
 	ids := b.routeTo(evt)
 	var errs []error
 	for _, id := range ids {
-		errs = append(errs, b.msh.Emit(id, evt))
+		errs = append(errs, b.emitter.Emit(id, evt))
 	}
 	return failure.Collect(errs...)
 }
 
 // Recover from an error.
-func (b *meshRouterBehavior) Recover(err interface{}) error {
+func (b *routerBehavior) Recover(err interface{}) error {
 	return nil
 }
 
